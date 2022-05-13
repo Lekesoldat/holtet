82b31b0f19c5efc000ca48473cbd113ccb6f2aed
Magnus L. Holtet <dev@holtet.me>
2022-05-13 14:04:23 +0200

    SpaceX animation

--- web/src/lib/components/Me.svelte
+++ web/src/lib/components/Me.svelte
@@ -2,9 +2,10 @@
 	<div>
 		<h2 class="text-2xl lg:text-4xl mb-6">👋, I'm Magnus!</h2>
 		<p class="text-md lg:text-lg">
-			I'm a software engineering student and an aspiring full stack developer who is addicted to
-			learning, snowboarding, and other adrenaline-rushing activities. I love working with people,
-			and enjoy <span class="font-bold">exciting projects</span> with cutting edge technologies!
+			I'm a software engineering student and an aspiring full stack developer who loves learning,
+			snowboarding, and other adrenaline-rushing activities. I like working with people, and enjoy <span
+				class="font-bold">exciting projects</span
+			> with cutting edge technologies!
 		</p>
 	</div>
 	<img class="hidden lg:block w-56 ml-16" src={'hi.png'} alt="Movie" />
