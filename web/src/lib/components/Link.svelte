8fe1b23ca393db625ab40b20a692380f99baf517
Magnus L. Holtet <dev@holtet.me>
2022-05-13 18:21:01 +0200

    remove console.log

--- web/src/lib/components/Link.svelte
+++ web/src/lib/components/Link.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	export let href: string;
-
-	console.log($page.url);
 </script>
 
 <a {href} class="uppercase lg:text-lg styled-link" class:active-link={$page.url.pathname === href}
