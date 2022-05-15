a5019173a7dd5b0a2dc77fdf0834452bf9d0e3ca
Magnus L. Holtet <dev@holtet.me>
2022-05-15 15:01:59 +0200

    Tilt L

--- web/src/lib/components/layout/Nav.svelte
+++ web/src/lib/components/layout/Nav.svelte
@@ -25,7 +25,7 @@
 
 <nav class="navbar justify-center flex-col mb-16 bg-base-100 lg:sticky lg:top-0 lg:z-50">
 	<h1 class="text-2xl lg:text-5xl font-bold mb-5">
-		<a href="/">Magnus <span class="text-primary">L.</span> Holtet</a>
+		<a href="/">Magnus <span class="text-primary inline-block -rotate-12">L.</span> Holtet</a>
 	</h1>
 
 	<ul class="w-full flex flex-wrap justify-around">
