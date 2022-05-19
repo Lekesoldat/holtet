fc42b02c73931199b77c131983b9bff8a0afbc08
Magnus L. Holtet <dev@holtet.me>
2022-05-19 22:26:18 +0200

    Update casing...

---
+++ web/src/lib/components/timeline/Timeline.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { TimelineEntryData } from '@lib/types/timeline';
+	import { ascendngDateSort } from './sorting-util';
+	import TimelineEntry from './TimelineEntry.svelte';
+
+	export let data: TimelineEntryData[];
+
+	$: sortedData = data.sort(ascendngDateSort).reverse();
+</script>
+
+<ol class="ml-4 relative border-l border-gray-200 dark:border-gray-700">
+	{#each sortedData as entry}
+		<TimelineEntry {entry} />
+	{/each}
+</ol>
