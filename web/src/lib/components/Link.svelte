9c007b84e7e826d775331c2dd3e8316ebb3e72cd
Magnus L. Holtet <dev@holtet.me>
2022-05-26 17:23:39 +0200

    Blindly update active link

--- web/src/lib/components/Link.svelte
+++ web/src/lib/components/Link.svelte
@@ -3,6 +3,9 @@
 	export let href: string;
 </script>
 
-<a sveltekit:prefetch {href} class="uppercase lg:text-lg styled-link" class:active-link={$page.url.pathname === href}
-	><slot /></a
+<a
+	sveltekit:prefetch
+	{href}
+	class="uppercase lg:text-lg styled-link"
+	class:active-link={$page.url.pathname.includes(href)}><slot /></a
 >
