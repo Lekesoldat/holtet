543b208befbc055137ed01f5794f8c1c1dda73a1
Magnus L. Holtet <dev@holtet.me>
2022-05-27 15:56:14 +0200

    Add highlighting

--- web/src/lib/components/portable-text/Code.svelte
+++ web/src/lib/components/portable-text/Code.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
 	import type { CustomBlockComponentProps } from '@portabletext/svelte';
+	import Prism from 'prismjs';
+	import 'prism-svelte';
 
 	export let portableText: CustomBlockComponentProps<{
 		code: string;
 		language: string;
 	}>;
 
-	$: ({ value } = portableText);
+	$: ({ code, language } = portableText.value);
+	$: html = Prism.highlight(code, Prism.languages[language], language);
 </script>
 
-<!-- TODO: Fix syntax highlighting -->
-<pre>
-  <p>{value.language}</p>
-  <code>{value.code}</code>
+<svelte:head>
+	<!-- PrismJS -->
+	<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism.min.css"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<pre class={`language-${language}`}>
+<code>
+{@html html}
+</code>
 </pre>
